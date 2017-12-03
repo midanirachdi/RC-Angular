cfdd6e4a16a3e0011a05fd97b4ecf983a42595d5
Midani Rachdi <midani.rachdi@esprit.tn>
2017-12-03 21:55:47 +0100

    restructure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,27 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './template/app-header/app-header.component';
-import { SliderComponent } from './template/slider/slider.component';
-import { ImageboxComponent } from './template/imagebox/imagebox.component';
+import { SliderComponent } from './template/homepage/slider/slider.component';
+import { ImageboxComponent } from './template/homepage/imagebox/imagebox.component';
 import { NavbarComponent } from './template/navbar/navbar.component';
-import { VideosectionComponent } from './template/videosection/videosection.component';
-import { EventsectionComponent } from './template/eventsection/eventsection.component';
-import { CallforactionsectionComponent } from './template/callforactionsection/callforactionsection.component';
-import { BlogsectionComponent } from './template/blogsection/blogsection.component';
-import { TestimonialsectionComponent } from './template/testimonialsection/testimonialsection.component';
+import { VideosectionComponent } from './template/homepage/videosection/videosection.component';
+import { EventsectionComponent } from './template/homepage/eventsection/eventsection.component';
+import { CallforactionsectionComponent } from './template/homepage/callforactionsection/callforactionsection.component';
+import { BlogsectionComponent } from './template/homepage/blogsection/blogsection.component';
+import { TestimonialsectionComponent } from './template/homepage/testimonialsection/testimonialsection.component';
 import { FooterComponent } from './template/footer/footer.component';
-import { VolunteersectionComponent } from './template/volunteersection/volunteersection.component';
+import { VolunteersectionComponent } from './template/homepage/volunteersection/volunteersection.component';
 import {RouterModule, Routes} from "@angular/router";
 import {ButtonModule} from "primeng/primeng";
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { JobOffersComponent } from './job-offers/job-offers.component';
+import { HomepageComponent } from './template/homepage/homepage.component';
+import { ProfileComponent } from './template/profile/profile.component';
 
 const tabRoute:Routes=[
+  {path:"",component:HomepageComponent},
   {path:"jobOffers",component:JobOffersComponent},
 
 ];
@@ -39,7 +42,9 @@ const tabRoute:Routes=[
     TestimonialsectionComponent,
     FooterComponent,
     VolunteersectionComponent,
-    JobOffersComponent
+    JobOffersComponent,
+    HomepageComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
