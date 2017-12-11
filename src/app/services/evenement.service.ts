6cefd8b7af419e9f9342f86f12d8c6fe22bbaa64
Khalil Fakhfekh <khalil.fakhfekh@esprit.tn>
2017-12-11 14:34:06 +0100

    evenements a venir page daccueil"

--- src/app/services/evenement.service.ts
+++ src/app/services/evenement.service.ts
@@ -1,3 +1,4 @@
+import { User } from './../entities/User';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Evenement } from '../entities/Evenement';
 import { Injectable } from '@angular/core';
@@ -5,15 +6,13 @@ import {Observable} from 'rxjs/Observable';
 @Injectable()
 export class EvenementService {
   // tslint:disable-next-line:max-line-length
-  token = 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJpZCI6Mywicm9sZSI6IkNhbXBDaGVmIn0.W8_n4zNk3-7wGttYWkSreT6wuLtkemuKJ3-Pq9_vZJhSjQfa2NLaPCtGFZRk0LbrBKaar3k4ApTS_jdwkUcH5Q';
   url= 'http://localhost:18080/refugeesCamp-web/api/evenements';
   header: HttpHeaders;
   httpOptions = {
-    headers: new HttpHeaders({ 'Authorization': this.token , 'Content-Type': 'application/json'})
+    headers: new HttpHeaders({ 'Content-Type': 'application/json'})
   };
   constructor(private http: HttpClient) {
     this.header = new HttpHeaders();
-    this.header.set('Authorization', this.token);
     this.header.set('Content-Type', 'application/json');
 
   }
@@ -22,8 +21,15 @@ export class EvenementService {
     return this.http.get<Evenement[]>(this.url);
   }
 
-  public GetEvenement(id) : Observable<Evenement[]>{
-    return this.http.get<Evenement[]>(this.url+"/"+id);
+  public GetEvenement(id) : Observable<Evenement>{
+    return this.http.get<Evenement>(this.url+"/"+id);
+  }
+
+  public GetMyEvents(id) : Observable<Evenement[]>{
+    return this.http.get<Evenement[]>(this.url+"/myevents/"+id);
+  }
+  public GetCreator(id) : Observable<User>{
+    return this.http.get<User>(this.url+"/creator/"+id);
   }
   public GetUpcoming() : Observable<Evenement[]>{
     return this.http.get<Evenement[]>(this.url+"/upcoming");
